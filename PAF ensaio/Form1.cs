ab044ea1d22434e78e65f058d1ffab2f8c4b2b52
baracinha <francisco.baracinha@gmail.com>
2026-03-16 21:47:08 +0000

    Adicionar arquivos de projeto.

---
+++ PAF ensaio/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace PAF_ensaio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM utilizadores where username = @u and password = @p";
+            MySqlParameter[] p = { new MySqlParameter("@u", textBox1.Text), new MySqlParameter("@p", textBox2.Text) };
+
+            DataTable dt = internalAPI.Consulta(sql, p);
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Login bem sucedido!, bem vindo " );
+            }
+            else
+            {
+                MessageBox.Show("Credenciais inválidas.");
+            }
+        }
+    }
+}
---
+++ PAF ensaio/internalAPI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace PAF_ensaio
+{
+    internal class internalAPI
+    {
+        private static string conexao = System.Configuration.ConfigurationManager.ConnectionStrings["conexao"].ConnectionString;
+
+        public static DataTable Consulta(string sql, MySqlParameter[] parametros = null)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                {
+                    if (parametros != null)
+                    {
+                        cmd.Parameters.AddRange(parametros);
+                    }
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+        public static int Executar(string sql, MySqlParameter[] parametros = null)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                {
+                    if (parametros != null) cmd.Parameters.AddRange(parametros);
+
+                    try
+                    {
+                        conn.Open();
+                        return cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        // Log ou tratamento de erro
+                        throw new Exception("Erro ao executar comando: " + ex.Message);
+
+
+                    }
+                }
+            }
+        }
+    }
+}
